248dd085ad17057e13386258241e0cfc406ae14b
Anders Lindgren <anders@lindgren.dk>
2017-10-25 12:29:39 +0000

    Adjusted init.js

--- src/public/index.php
+++ src/public/index.php
@@ -1,9 +1,10 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set("display_errors",1);
+
 require '../vendor/autoload.php';
 
-    error_reporting(E_ALL);
-    ini_set("display_errors",1);
 /*
 $dev = true;
 
